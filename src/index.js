d5757e2a7ad38406fd23310bd5a98e11ac44b893
Vladimir Riz <resin.fhp@gmail.com>
2020-09-27 21:39:28 +0300

    feat: function but need fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-    // your solution
+    const arr = [];
+    bracketsConfig.forEach((x) => {
+        for (let i in x) {
+            arr.push(x[i]);
+        }
+    });
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === arr[i]) {
+            continue;
+        } else return false;
+    }
+    return true;
 };
 
-const check = (str, bracketsConfig) => {};
+const check = (str, bracketsConfig) => {
+    const arr = [];
+    bracketsConfig.forEach((x) => {
+        for (let i in x) {
+            arr.push(x[i]);
+        }
+    });
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === arr[i]) {
+            continue;
+        } else return false;
+    }
+    return true;
+};
+
+console.log(check("()", [["(", ")"]]));
