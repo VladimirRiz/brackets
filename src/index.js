1b1f2f1bcebd26542b0945c2e054fbc3adc9248e
Vladimir Riz <resin.fhp@gmail.com>
2020-09-28 18:44:54 +0300

    refactor: add foreach instead of for

--- src/index.js
+++ src/index.js
@@ -3,53 +3,33 @@ module.exports = function check(str, bracketsConfig) {
     let s = str.split("");
 
     for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            if (
-                s[i] === bracketsConfig[j][0] &&
-                s[i + 1] === bracketsConfig[j][1]
-            ) {
-                s.splice(i, 2);
-                i -= 2;
+        bracketsConfig.forEach((x) => {
+            {
+                if (s[i] === x[0] && s[i + 1] === x[1]) {
+                    s.splice(i, 2);
+                    i -= 2;
+                }
             }
-        }
+        });
     }
     return !s.length;
 };
 
-// check = (str, bracketsConfig) => {
-//     if (str.length % 2) return false;
-//     // const flatSingle = (arr) => [].concat(...arr),
-//     // arr = flatSingle(bracketsConfig);
+check = (str, bracketsConfig) => {
+    if (str.length % 2) return false;
+    let s = str.split("");
 
-//     let s = str.split("");
+    for (let i = 0; i < s.length; i++) {
+        bracketsConfig.forEach((x) => {
+            {
+                if (s[i] === x[0] && s[i + 1] === x[1]) {
+                    s.splice(i, 2);
+                    i -= 2;
+                }
+            }
+        });
+    }
+    return !s.length;
+};
 
-//     for (let i = 0; i < s.length; i++) {
-//         for (let j = 0; j < bracketsConfig.length; j++) {
-//             // console.log(s[i], arr[j]);
-//             if (
-//                 s[i] === bracketsConfig[j][0] &&
-//                 s[i + 1] === bracketsConfig[j][1]
-//             ) {
-//                 // console.log(s[i], s[i + 1], "w", arr[j], arr[j + 1]);
-//                 s.splice(i, 2);
-//                 i -= 2;
-//             }
-//         }
-//     }
-//     // console.log(s);
-//     // return s.length % 2;
-//     return !s.length;
-// };
-
-// console.log(
-//     check(
-//         "8888877878887777777888888887777777887887788788887887777777788888888887788888",
-//         [
-//             ["1", "2"],
-//             ["3", "4"],
-//             ["5", "6"],
-//             ["7", "7"],
-//             ["8", "8"],
-//         ]
-//     )
-// );
+console.log(check("()", [["(", ")"]]));
